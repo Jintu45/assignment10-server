f8f264280f4ebcfa8ce2f763e79c1b1ff9bed05e
Zintu paul <jintupaul45@gmail.com>
2022-10-26 12:05:48 -0700

    some mistake solve

--- index.js
+++ index.js
@@ -6,22 +6,27 @@ const cors = require('cors')
 app.use(cors())
 const categories = require('./data/Category.json')
 const tutorials = require('./data/Data.json')
+
 app.get('/', (req, res) => {
     res.send('hello bangladesh')
 })
 app.get('/categories_item', (req, res) => {
     res.send(categories)
 })
+,
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
     const tutorialCategory = tutorials.filter(tutorial => tutorial.category_id === id);
     res.send(tutorialCategory)
 })
-app.get('/tutorial/:id', (req, res) => {
+
+app.get('/tutorials/:id', (req, res) => {
+    console.log(req.params)
     const id = req.params.id;
     const selectTutorial = tutorials.find(tutorial => tutorial._id === id)
     res.send(selectTutorial)
 })
+
 app.listen(port, ()=> {
     console.log('server running now', port)
 })
